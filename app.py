44e208b62b76fc6d98aeb78d0d0bac3570b3f331
RajaLakshmi-RP <rajeeram7@gmail.com>
2025-06-20 23:29:16 -0400

    Message Updated

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # Trigger GitHub Actions build
 def hello():
-    return "Hello, Docker + GitHub Actions!"
+    return "Hello World! , It's Docker + GitHub Actions!"
 
 if __name__ == "__main__":
     hello()
